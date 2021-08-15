de9f2c317812baceb454d008a43163c846901b92
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-08-15 14:52:13 +0530

    contact form done

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,33 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <p>Name:</p>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Contact Name"
+      />
+      <p>Phone:</p>
+      <input
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$"
+        required
+        placeholder="Contact Phone Number"
+      />
+      <p>Email:</p>
+      <input
+        type="tel"
+        value={email}
+        onchange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Contact Email"
+      />
+      <input type="submit" value="Add Contact" />
+    </form>
   );
 };
