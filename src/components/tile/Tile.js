dbad203b7a0317a12facc68cf1a9f874f9df1eeb
athul jyothis <63841734+athulJ18@users.noreply.github.com>
2021-08-15 15:12:19 +0530

    tile done

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
-
-export const Tile = () => {
+// revieve tile and just display it 
+export const Tile = ({ tile }) => {
   return (
     <div className="tile-container">
-      
+      {Object.values(tile).map((value, index) => (
+        <p 
+        key={index} 
+        className={`${index === 0 ? "tile-title" : ""} tile`}>
+        {value}
+        </p>
+      ))}
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Tile } from "../tile/Tile";
-
+// Recieve tiles(contacts or appointments) and pass it as tile (contact or appointment) to Tile component
 export const TileList = ({ tiles }) => {
   return (
     <div>
